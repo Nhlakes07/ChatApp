3f4c9ef4b926ae79cd9b4f244fbda69892ecb9ab
Mpho Malapile <59501013+mphomalapile@users.noreply.github.com>
2024-02-13 10:41:21 +0200

    deleted version

--- app/src/main/java/com/example/myapplication/listeners/UserListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.listeners;
-
-import com.example.myapplication.models.Users;
-
-public interface UserListener
-{
-    void onUserClicked(Users users);
-}
--- app/src/main/java/com/example/myapplication/models/Users.java
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.models;
-
-import java.io.Serializable;
-
-public class Users implements Serializable
-{
-    public String Name,Surname,Token ,Email,id,PhoneNumber,image;
-}
